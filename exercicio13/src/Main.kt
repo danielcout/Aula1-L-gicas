3a0df6b75357f3d321b4833aa798d1c0815484f1
danielcout <125165311+danielcout@users.noreply.github.com>
2024-02-27 08:19:26 -0300

    Add files via upload

---
+++ exercicio13/src/Main.kt
@@ -0,0 +1,13 @@
+package fundamentos.controles//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    val nota: Int =4
+    when(nota){
+        10, 9 -> print("Fantástico")
+        8, 7 -> print("Parabéns")
+        6, 5, 4 ->print("Tem como recuperar")
+        in 3..0 -> print("Te vejo no próximo ano")
+        else -> print("Nota inválida")
+
+    }
+}
